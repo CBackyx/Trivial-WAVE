a1a972f6057c7b3605acbb7bfb8b1226a1f26afa
CBackyx <cbackyx@gmail.com>
2021-12-13 12:55:50 +0000

    Add prove and verify

--- main.py
+++ main.py
@@ -130,26 +130,108 @@ def dispatch_args(args):
     elif action == "prove":
         if not (subject and policy and target):
             raise Exception("Lack prove args")
+
+        attest_sk, attest_pk, sign_sk, sign_pk = local_load_keys(subject)
+        policy_sk, policy_ID = attest_sk.extract(policy)
+        policy_pk = attest_sk.n
+        cocks = Cocks(policy_pk)
+
+        transactor = Transactor()
+        enc_attest = transactor.getCert(pickle.dumps(subject) + pickle.dumps(policy), 0)
+
+        attest_path = []
+
+        while len(enc_attest) != 0:
+            attest = cocks.decrypt(enc_attest, policy_sk, policy_ID)
+            attest = pickle.loads(attest)
+            if not isinstance(attest, dict):
+                raise Exception("Error decrypted attestation type")
+            if not "Cert-content" in attest:
+                raise Exception("Error decrypted attestation dict, no certificate")
+
+            # Verify certificate integrity
+            if not verify_cert(attest["Cert-content"], attest["Signature"], attest["Cert-content"]["Issuer"], transactor): 
+                raise Exception("Error attestation signature")
+
+            policy_pk = attest.pop("Issuer-policy-sk", None)
+
+            attest_path.push(attest)
+
+            # Obtain encrypted attestation (for the target policy) issued the the issuer
+            enc_attest = transactor.getCert(pickle.dumps(attest["Cert-content"]["Issuer"]) + pickle.dumps(policy), 0)   
+
+        # Store attest_path as the proof
+        local_save_proof(subject, policy, attest_path)
+
         pass
 
     elif action == "verify":
-        if not cert:
+        if not (subject and policy):
             raise Exception("Lack verify args")
+
+        transactor = Transactor()
+
+        # Locally load the proof, assuming that the proof has been recieved and stored secretly
+        attest_path = local_load_proof(subject, policy)
+
+        time_range_begin = -1
+        time_range_end = 2**64
+
+        for attest in attest_path:
+            assert(attest["Cert-content"]["Subject"] == subject, "Error (not matched) subject in attest path")
+
+            # Verify certificate integrity
+            if not verify_cert(attest["Cert-content"], attest["Signature"], attest["Cert-content"]["Issuer"], transactor): 
+                raise Exception("Error attestation signature")
+
+            # Verify the time range
+            if not (attest["Cert-content"]["Time-range"][0] >= time_range_begin and attest["Cert-content"]["Time-range"][1] <= time_range_end):
+                raise Exception("Conflicting time range for the policy")
+
+            assert(attest["Cert-content"]["Time-range"][0] < attest["Cert-content"]["Time-range"][1], "Begin of time range larger than the end")         
+
+            time_range_begin = attest["Cert-content"]["Time-range"][0]
+            time_range_end = attest["Cert-content"]["Time-range"][1]
+
+            subject = attest["Cert-content"]["Issuer"]
+
+        assert(isAuthority(subject), "The end of the proof path is not an authority")
+
         pass
 
     else:
         raise Exception("Unsupported action")
         pass
 
+def isAuthority(entity):
+    authority_list = ["admin"]
+    return (entity in authority_list)
+
+def verify_cert(cert, sign, signer, transactor):
+    cert_str = json.dumps(cert)
+    # Obtain the signer public key from the chain
+    sign_pk = pickle.loads(transactor.getEntitySignPubKey(pickle.dumps(signer)))
+    return signature.verify(cert_str, sign, sign_pk)
+
 def local_save_keys(entityname, key_list):
     # Locally store keys
-    with open("local_storage/" + entityname + ".obj", "wb") as keyfile:
+    with open("local_storage/" + entityname + ".keystore", "wb") as keyfile:
         pickle.dump(key_list, keyfile)    
 
 def local_load_keys(entityname):
-    key_list = load_all("local_storage/" + entityname + ".obj")
+    key_list = load_all("local_storage/" + entityname + ".keystore")
     keys = list(key_list)[0]
     return keys
+
+def local_save_proof(entityname, policy, proof):
+    # Locally store proof
+    with open("local_storage/" + entityname + policy + ".proof", "wb") as pfile:
+        pickle.dump(proof, pfile)    
+
+def local_load_proof(entityname, policy):
+    proof_list = load_all("local_storage/" + entityname + policy + ".proof")
+    proof = list(proof_list)[0]
+    return proof
 
 def load_all(filename):
     with open(filename, "rb") as ifile:
@@ -171,7 +253,7 @@ def main():
     arg_parser.add_argument("-r", "--timerange", type=str, help="policy time range, formatted as [begin]:[end], of which the time format is month/day/year")
     arg_parser.add_argument("-t", "--target", type=str, help="permission prove target")
 
-    arg_parser.add_argument("-c", "--certificate", type=str, help="certificate for permission verification")
+    arg_parser.add_argument("-c", "--proof", type=str, help="proof for permission verification")
 
     args = arg_parser.parse_args()
 
